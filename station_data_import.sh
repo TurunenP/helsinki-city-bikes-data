2784c2ac010d9b83121b3aa464839fdaf0a72365
TurunenP <k7jepu00@students.oamk.fi>
2023-01-15 20:48:31 +0300

    added a bash script to import stations data to mongodb in mongo cluster

---
+++ station_data_import.sh
@@ -0,0 +1,3 @@
+mongoimport --uri "mongodb+srv://doadmin:3Cn2Kp4jcJ80W659@db-mongodb-nyc1-22526-ccc29d80.mongo.ondigitalocean.com/admin?tls=true&authSource=admin" --collection stations --jsonArray --type json  --file station_data.json
+
+
